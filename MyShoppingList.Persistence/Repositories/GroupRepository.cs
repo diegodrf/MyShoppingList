34fca94d580ae0b6c7c40aeef8b615a8c9d44ac5
Diego Faria <diego.rdfaria@gmail.com>
2025-04-20 03:06:26 -0400

    Repositories

---
+++ MyShoppingList.Persistence/Repositories/GroupRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyShoppingList.Application.Ports.Secondary;
+using MyShoppingList.Domain.Entities;
+
+namespace MyShoppingList.Persistence.Repositories;
+public class GroupRepository : IGroupRepository
+{
+    private readonly MyShoppingListDbContext _db;
+    public GroupRepository(MyShoppingListDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<Group> CreateAsync(Group group, CancellationToken cancellationToken)
+    {
+        await _db.Groups.AddAsync(group, cancellationToken);
+        await _db.SaveChangesAsync(cancellationToken);
+        return group;
+    }
+
+    public Task<List<Group>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return _db.Groups
+            .AsSplitQuery()
+            .Include(g => g.ItemGroups.Where(x => x.Item!.RemovedAt == null))
+            .ThenInclude(i => i.Item)
+            .Where(g => g.RemovedAt == null)
+            .ToListAsync(cancellationToken);
+    }
+
+    public Task<Group?> GetByIdAsync(int id, CancellationToken cancellationToken)
+    {
+        return _db.Groups
+            .AsSplitQuery()
+            .Include(g => g.ItemGroups.Where(x => x.Item!.RemovedAt == null))
+            .ThenInclude(i => i.Item)
+            .Where(g => g.RemovedAt == null)
+            .Where(g => g.Id == id)
+            .FirstOrDefaultAsync(cancellationToken);
+    }
+
+    public async Task RemoveItemAsync(int groupId, int ItemId, CancellationToken cancellationToken)
+    {
+        await _db.ItemGroups
+            .Where(x => x.GroupId == groupId)
+            .Where(x => x.ItemId == ItemId)
+            .ExecuteDeleteAsync(cancellationToken);
+    }
+
+    public Task UpdateAsync(Group group, CancellationToken cancellationToken)
+    {
+        _db.Groups.Update(group);
+        return _db.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ MyShoppingList.Persistence/Repositories/ItemRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyShoppingList.Application.Ports.Secondary;
+using MyShoppingList.Domain.Entities;
+
+namespace MyShoppingList.Persistence.Repositories;
+public class ItemRepository : IItemRepository
+{
+    private readonly MyShoppingListDbContext _dbContext;
+
+    public ItemRepository(MyShoppingListDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Item> CreateAsync(Item item, CancellationToken cancellationToken)
+    {
+        await _dbContext.Items.AddAsync(item, cancellationToken);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+        return item;
+    }
+
+    public Task<Item?> GetByIdAsync(int id, CancellationToken cancellationToken)
+    {
+        return _dbContext.Items
+            .AsSplitQuery()
+            .Include(i => i.ItemGroups.Where(x => x.Group!.RemovedAt == null))
+            .ThenInclude(g => g.Group)
+            .Where(i => i.RemovedAt == null)
+            .Where(i => i.Id == id)
+            .FirstOrDefaultAsync(cancellationToken);
+    }
+
+    public async Task<Item> UpdateAsync(Item item, CancellationToken cancellationToken)
+    {
+        _dbContext.Items.Update(item);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+        return item;
+    }
+}
